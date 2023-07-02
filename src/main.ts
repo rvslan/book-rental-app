4fa09e668d3f66978066509e1231a22b8b722082
Ruslan <rvslan@pm.me>
2023-07-02 18:32:53 +0200

    Added some comments on main

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,31 @@
-import { ValidationPipe } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Create a Nest application
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("/api");
+
+  // Set global prefix for API routes
+  app.setGlobalPrefix('/api');
+
+  // Get the ConfigService instance
   const configService = app.get(ConfigService);
+
+  // Apply global validation pipe with transform option enabled
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  
+
+  // Enable Cross-Origin Resource Sharing (CORS)
   app.enableCors({
     origin: true,
-    methods: ["*"],
+    methods: ['*'],
     credentials: true,
   });
-  await app.listen(configService.get<number>("port"));
-  console.log(configService.get<number>("port"));
+
+  // Start listening on the configured port
+  await app.listen(configService.get<number>('port'));
 }
 
+// Bootstrap the application
 bootstrap();
