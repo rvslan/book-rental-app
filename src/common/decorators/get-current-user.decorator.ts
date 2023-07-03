183cbbb248ed0bb6e71d95a5ba3edf8259c08d44
Ruslan <rvslan@pm.me>
2023-07-03 19:53:45 +0200

    Handled user retrievement on GetCurrentUser

--- src/common/decorators/get-current-user.decorator.ts
+++ src/common/decorators/get-current-user.decorator.ts
@@ -1,10 +1,28 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { JwtPayloadWithRt } from '../../auth/types';
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { PrismaClient, User as UserModel } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const GetCurrentUser = createParamDecorator(
-  (data: keyof JwtPayloadWithRt | undefined, context: ExecutionContext) => {
+  async (
+    data: string | undefined,
+    context: ExecutionContext
+  ): Promise<UserModel> => {
     const request = context.switchToHttp().getRequest();
-    if (!data) return request.user;
-    return request.user[data];
-  },
+
+    // Retrieve the user from the database using Prisma
+    const user = await prisma.user.findUnique({
+      where: {
+        id: request.user["sub"],
+      },
+    });
+
+    // If user is not found, throw an unauthorized exception
+    if (!user) {
+      throw new UnauthorizedException("Authenticate first");
+    }
+
+    // Return the user object
+    return user;
+  }
 );
