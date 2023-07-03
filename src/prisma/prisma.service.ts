64aa5ec18e820e98d37837da0f19b92419ec45c2
Ruslan <rvslan@pm.me>
2023-07-03 19:53:06 +0200

    Optimized PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '.prisma/client';
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from ".prisma/client";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class PrismaService
@@ -8,12 +8,10 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor(config: ConfigService) {
-    const url = config.get<string>('DATABASE_URL');
-
     super({
       datasources: {
         db: {
-          url,
+          url: config.get<string>("DATABASE_URL"),
         },
       },
     });
@@ -28,7 +26,7 @@ export class PrismaService
   }
 
   async cleanDatabase() {
-    if (process.env.NODE_ENV === 'production') return;
+    if (process.env.NODE_ENV === "production") return;
 
     // teardown logic
     return Promise.all([this.user.deleteMany()]);
